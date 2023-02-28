Layernorm bugfix regarding tensor sizing/broadcasting. GPT2 demo updated to include layernorms and residual connection

src/tt_functional.py
@@ -293,7 +293,7 @@ def layer_norm(lin, beta, gamma, op_dtype = tt_op_dtype(tt_dtype.Float16), runti
     reduce_fold_factors[1] = 1 # We can't fold columns on the reduction, or the reduced tensor going into reciprocal
     sumo = reduce(lin, dim=-1, op_dtype=op_dtype, runtime=runtime, fold_factors=tuple(reduce_fold_factors))
     sumo = sumo.unsqueeze(-1)
-    numel = torch.fill(torch.empty((1,1,1,lin.block_size, lin.block_size)), 1/(lin.shape[-1]*lin.block_size)) # tensor filled w/ number of elements in reduce dim
+    numel = torch.fill(torch.empty((1,lin.block_size, lin.block_size)), 1/(lin.shape[-1]*lin.block_size)) # tensor filled w/ number of elements in reduce dim
     tt_numel = tt_tensor(lin.block_size, runtime.simd_cluster, torch_tensor=numel, dtype=lin.dtype).to_device(0, numel)
     avg = multiply(sumo, tt_numel, op_dtype=op_dtype, runtime=runtime, fold_factors=fold_factors)
     # VARIANCE
@@ -301,10 +301,10 @@ def layer_norm(lin, beta, gamma, op_dtype = tt_op_dtype(tt_dtype.Float16), runti
     sqr = multiply(diff, diff, op_dtype=op_dtype, runtime=runtime, fold_factors=fold_factors)
     var_unnormalized = reduce(sqr, dim=-1, op_dtype=op_dtype, runtime=runtime, fold_factors=tuple(reduce_fold_factors))
     # TODO: divide by numel to compute variance
+    var_unnormalized = var_unnormalized.unsqueeze(-1)
     var = multiply(var_unnormalized, tt_numel, op_dtype=op_dtype, runtime=runtime, fold_factors=fold_factors)
     sqrto = sqrt(var, op_dtype=op_dtype, runtime=runtime, fold_factors=tuple(reduce_fold_factors))
     recip = reciprocal(sqrto, op_dtype=op_dtype, runtime=runtime, fold_factors=tuple(reduce_fold_factors))
-    recip = recip.unsqueeze(-1)
     scaled_diff = multiply(diff, recip, op_dtype=op_dtype, runtime=runtime, fold_factors=fold_factors)
     g_scaled = multiply(scaled_diff, gamma, op_dtype=op_dtype, runtime=runtime, fold_factors=fold_factors)
     out = add(g_scaled, beta, op_dtype=op_dtype, runtime=runtime, fold_factors=fold_factors)

tests/test_gpt2.py
@@ -39,27 +39,44 @@ def tt_gpt2(target_arch):
     simd.set_up_allocators([(dtype, block_size, 2000, 250000000)])
 
     '''
-    Q, K, V projections
+    LayerNorm
     '''
-
-    # TODO: pre-norm X
-
     s, dm, dh, nh = 256, 1024, 1024//4, 4
     x = torch.randn(1, s, dm)
+
+    normalized_shape = dm
+    gamma_ln1 = torch.randn(normalized_shape)
+    beta_ln1 = torch.randn(normalized_shape)
+    x_norm = torch.nn.functional.layer_norm(x, gamma_ln1.size(), weight=gamma_ln1, bias=beta_ln1, eps=1e-05)
+
+    tt_x = tt_tensor(block_size, simd, torch_tensor=x, dtype=dtype).to_device(0, x)
+    gamma_ln1_expand = gamma_ln1.expand(x.size()) # I think we need to make this the same size as input for tt_functional
+    tt_gamma_ln1 = tt_tensor(block_size, simd, torch_tensor=gamma_ln1_expand, dtype=dtype).to_device(0, gamma_ln1_expand)
+    beta_ln1_expand = beta_ln1.expand(x.size())
+    tt_beta_ln1 = tt_tensor(block_size, simd, torch_tensor=beta_ln1_expand, dtype=dtype).to_device(0, beta_ln1_expand)
+
+    tt_x_norm = ttf.layer_norm(tt_x, beta=tt_beta_ln1, gamma=tt_gamma_ln1, op_dtype=op_dtype, runtime=runtime, fold_factors=(1,1,1))
+    tt_x_norm_cpu = tt_x_norm.from_device(0)
+
+    logging.info(f'LN MAFE: {mean_absolute_fraction_error(x_norm, tt_x_norm_cpu):.3f}')
+
+    '''
+    Q, K, V projections
+    '''
     Wq, Wv, Wk = (torch.randn(1, dm, dm) for _ in range(3))
-    q = torch.matmul(x, Wq) # Ignore bias and weight tranposing
-    k = torch.matmul(x, Wk)
-    v = torch.matmul(x, Wv)
+    q = torch.matmul(x_norm, Wq) # Ignore bias and weight tranposing
+    k = torch.matmul(x_norm, Wk)
+    v = torch.matmul(x_norm, Wv)
 
     # Pushing inputs to device
-    tt_x = tt_tensor(block_size, simd, torch_tensor=x, dtype=dtype).to_device(0, x)
+    
     tt_Wq = tt_tensor(block_size, simd, torch_tensor=Wq, dtype=dtype).to_device(0, Wq)
     tt_Wk = tt_tensor(block_size, simd, torch_tensor=Wk, dtype=dtype).to_device(0, Wk)
     tt_Wv = tt_tensor(block_size, simd, torch_tensor=Wv, dtype=dtype).to_device(0, Wv)
 
-    tt_q = ttf.matmul(tt_x, tt_Wq, op_dtype=op_dtype, runtime=runtime)
-    tt_k = ttf.matmul(tt_x, tt_Wk, op_dtype=op_dtype, runtime=runtime)
-    tt_v = ttf.matmul(tt_x, tt_Wv, op_dtype=op_dtype, runtime=runtime)
+    tt_q = ttf.matmul(tt_x_norm, tt_Wq, op_dtype=op_dtype, runtime=runtime)
+    tt_k = ttf.matmul(tt_x_norm, tt_Wk, op_dtype=op_dtype, runtime=runtime)
+    tt_v = ttf.matmul(tt_x_norm, tt_Wv, op_dtype=op_dtype, runtime=runtime)
 
     tt_q_cpu = tt_q.from_device(0)
     tt_k_cpu = tt_k.from_device(0)
@@ -123,26 +140,52 @@ def tt_gpt2(target_arch):
 
     print('dense out MAFE:', mean_absolute_fraction_error(dense_out, tt_dense_out.from_device(0)))
 
+
     '''
-    MLP
+    Residual & LayerNorm
+    '''
+    attn_res = x + dense_out
+
+    gamma_ln2 = torch.randn(normalized_shape)
+    beta_ln2 = torch.randn(normalized_shape)
+    attn_res_norm = torch.nn.functional.layer_norm(attn_res, gamma_ln2.size(), weight=gamma_ln2, bias=beta_ln2, eps=1e-05)
+
+    gamma_ln2_expand = gamma_ln2.expand(x.size()) # I think we need to make this the same size as input for tt_functional
+    tt_gamma_ln2 = tt_tensor(block_size, simd, torch_tensor=gamma_ln2_expand, dtype=dtype).to_device(0, gamma_ln2_expand)
+    beta_ln2_expand = beta_ln2.expand(x.size())
+    tt_beta_ln2 = tt_tensor(block_size, simd, torch_tensor=beta_ln2_expand, dtype=dtype).to_device(0, beta_ln2_expand)
+
+    tt_attn_res = ttf.add(tt_x, tt_dense_out, op_dtype=op_dtype, runtime=runtime)
+    tt_attn_res_norm = ttf.layer_norm(tt_attn_res, beta=tt_beta_ln2, gamma=tt_gamma_ln2, op_dtype=op_dtype, runtime=runtime, fold_factors=(1,1,1))
+    tt_attn_res_norm_cpu = tt_attn_res_norm.from_device(0)
+
+    print('Residual Norm MAFE:', mean_absolute_fraction_error(attn_res_norm, tt_attn_res_norm_cpu))
+    
+
+    '''
+    MLP & residual (TODO)
     '''
     ff1 = torch.randn(1, dm, 4*dm)
     ff2 = torch.randn(1, 4*dm, dm)
 
-    ff1_out = torch.matmul(dense_out, ff1)
+    ff1_out = torch.matmul(attn_res_norm, ff1)
     gelu_out = torch.nn.functional.gelu(ff1_out)
     ff2_out = torch.matmul(gelu_out, ff2)
+    gpt2_out = attn_res + ff2_out
 
     tt_ff1 = tt_tensor(block_size, simd, torch_tensor=ff1, dtype=dtype).to_device(0, ff1)
     tt_ff2 = tt_tensor(block_size, simd, torch_tensor=ff2, dtype=dtype).to_device(0, ff2)
 
-    tt_ff1_out = ttf.matmul(tt_dense_out, tt_ff1, op_dtype=op_dtype, runtime=runtime)
+    tt_ff1_out = ttf.matmul(tt_attn_res_norm, tt_ff1, op_dtype=op_dtype, runtime=runtime)
     tt_gelu_out = ttf.gelu(tt_ff1_out, op_dtype=op_dtype, runtime=runtime)
     tt_ff2_out = ttf.matmul(tt_gelu_out, tt_ff2, op_dtype=op_dtype, runtime=runtime)
+    tt_gpt2_out = ttf.add(tt_attn_res, tt_ff2_out, op_dtype=op_dtype, runtime=runtime)
 
-    tt_ff2_out_cpu = tt_ff2_out.from_device(0)
+    tt_gpt2_out_cpu = tt_gpt2_out.from_device(0)
 
-    print('attn out MAFE:', mean_absolute_fraction_error(ff2_out, tt_ff2_out_cpu))
+    print('gpt2 decoder out MAFE:', mean_absolute_fraction_error(gpt2_out, tt_gpt2_out_cpu))
+
+
 
     be_api.finish_child_process()
     backend.destroy()