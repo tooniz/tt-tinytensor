3acf7938d3516362fe01ef30b3335384b5719d7c
Colman Glagovich <cglagovich@tenstorrent.com>
2023-03-01 18:08:20 +0000

    Cleaned up gpt2 script even more

--- tests/test_gpt2.py
+++ tests/test_gpt2.py
@@ -26,6 +26,7 @@ def tt_gpt2(target_arch):
     '''
     Functional GPT2 decoder 
     '''
+    # TT setup
     simd = tt_simd_cluster(1, 1, [0,], be_api, arch=target_arch)
     target_devices = {0}
     config = be_api.get_runtime_config(target_arch)
@@ -35,12 +36,13 @@ def tt_gpt2(target_arch):
     runtime = tt_runtime(simd, netlist, be_api, backend)
     dtype = tt_dtype.Float32
     op_dtype = tt_op_dtype(dtype, dtype_intermed=dtype, dtype_accum=dtype)
-    block_size = 128 # can't be bigger than dhead, can't be so small we require 16x16 cores
+    block_size = 128
     simd.set_up_allocators([(dtype, block_size, 2000, 250000000)])
 
     '''
     LayerNorm
     '''
+    # PyTorch
     s, dm, dh, nh = 256, 1024, 1024//4, 4
     x = torch.randn(1, s, dm)
 
@@ -49,8 +51,9 @@ def tt_gpt2(target_arch):
     beta_ln1 = torch.randn(normalized_shape)
     x_norm = torch.nn.functional.layer_norm(x, gamma_ln1.size(), weight=gamma_ln1, bias=beta_ln1, eps=1e-05)
 
+    # TinyTensor
     tt_x = tt_tensor(block_size, simd, torch_tensor=x, dtype=dtype).to_device(0, x)
-    gamma_ln1_expand = gamma_ln1.expand(x.size()) # I think we need to make this the same size as input for tt_functional
+    gamma_ln1_expand = gamma_ln1.expand(x.size())
     tt_gamma_ln1 = tt_tensor(block_size, simd, torch_tensor=gamma_ln1_expand, dtype=dtype).to_device(0, gamma_ln1_expand)
     beta_ln1_expand = beta_ln1.expand(x.size())
     tt_beta_ln1 = tt_tensor(block_size, simd, torch_tensor=beta_ln1_expand, dtype=dtype).to_device(0, beta_ln1_expand)
@@ -58,18 +61,18 @@ def tt_gpt2(target_arch):
     tt_x_norm = ttf.layer_norm(tt_x, beta=tt_beta_ln1, gamma=tt_gamma_ln1, op_dtype=op_dtype, runtime=runtime, fold_factors=(1,1,1))
     tt_x_norm_cpu = tt_x_norm.from_device(0)
 
-    logging.info(f'LN MAFE: {mean_absolute_fraction_error(x_norm, tt_x_norm_cpu):.3f}')
+    logging.info(f'LN1 MAE: {mean_absolute_fraction_error(x_norm, tt_x_norm_cpu):.3f}')
 
     '''
     Q, K, V projections
     '''
+    # PyTorch
     Wq, Wv, Wk = (torch.randn(1, dm, dm) for _ in range(3))
-    q = torch.matmul(x_norm, Wq) # Ignore bias and weight tranposing
+    q = torch.matmul(x_norm, Wq)
     k = torch.matmul(x_norm, Wk)
     v = torch.matmul(x_norm, Wv)
 
-    # Pushing inputs to device
-    
+    # TinyTensor
     tt_Wq = tt_tensor(block_size, simd, torch_tensor=Wq, dtype=dtype).to_device(0, Wq)
     tt_Wk = tt_tensor(block_size, simd, torch_tensor=Wk, dtype=dtype).to_device(0, Wk)
     tt_Wv = tt_tensor(block_size, simd, torch_tensor=Wv, dtype=dtype).to_device(0, Wv)
@@ -83,18 +86,18 @@ def tt_gpt2(target_arch):
     tt_v_cpu = tt_v.from_device(0)
 
     for exp, rec in zip([q, k, v], [tt_q_cpu, tt_k_cpu, tt_v_cpu]):
-        logging.info('MAFE: ', mean_absolute_fraction_error(exp, rec))
+        logging.info(f'MAE: {mean_absolute_fraction_error(exp, rec):.3f}')
 
     '''
-    Q * K.T attention scores
+    Q * K.T attention probabilities
     '''
-    # s, d -> nh, s, dm
+    # PyTorch
     q = q.reshape(s, nh, dh).permute(1,0,2)
     k = k.reshape(s, nh, dh).permute(1,2,0)
     scores = torch.matmul(q, k)
     probs = torch.nn.functional.softmax(scores, dim=-1)
     
-    # reshape in terms of blocks
+    # TinyTensor
     tt_q = tt_q.reshape((s//block_size, nh, dm//block_size//nh)).swapaxes(-2, -3)
     
     tt_k = tt_k.reshape((s//block_size, nh, dm//block_size//nh)).swapaxes(-2, -3).transpose()
@@ -103,54 +106,60 @@ def tt_gpt2(target_arch):
 
     tt_scores_cpu = tt_scores.from_device(0)
     
-    logging.info(f"Scores MAFE: {mean_absolute_fraction_error(scores, tt_scores_cpu):.3f}%")
+    logging.info(f"Scores MAE: {mean_absolute_fraction_error(scores, tt_scores_cpu):.3f}")
 
-    # # We don't yet have stable softmax because reduce_max not supported. Do it from pytorch for now.
-    scores_max = torch.max(tt_scores_cpu, dim=-1, keepdim=True)[0].expand(tt_scores_cpu.size()) # TODO: do on device
+    # Interoperability with pytorch when desired
+    scores_max = torch.max(tt_scores_cpu, dim=-1, keepdim=True)[0].expand(tt_scores_cpu.size())
     tt_scores_max = tt_tensor(block_size, simd, torch_tensor=scores_max, dtype=dtype).to_device(0, scores_max)
     tt_scores_normed = ttf.subtract(tt_scores, tt_scores_max, op_dtype=op_dtype, runtime=runtime)
 
     tt_probs = ttf.softmax(tt_scores_normed, dim=-1, op_dtype=op_dtype, runtime=runtime, fold_factors=(1,1,1))
     tt_probs_cpu = tt_probs.from_device(0)
-    logging.info(f'Probabilities MAFE: {mean_absolute_fraction_error(probs, tt_probs_cpu):.3f}')
+
+    logging.info(f'Probabilities MAE: {mean_absolute_fraction_error(probs, tt_probs_cpu):.3f}')
 
     '''
-    probs * V
+    attention_probabilities * V
     '''
+    # PyTorch
     v = v.reshape(s, nh, dh).permute(1,0,2)
     attn_out = torch.matmul(probs, v)
 
+    # TinyTensor
     tt_v = tt_v.reshape((s//block_size, nh, dm//block_size//nh)).swapaxes(-2, -3)
     tt_attn_out = ttf.matmul(tt_probs, tt_v, op_dtype=op_dtype, runtime=runtime)
     tt_attn_out_cpu = tt_attn_out.from_device(0)
 
-    print('attn out MAFE:', mean_absolute_fraction_error(attn_out, tt_attn_out_cpu))
+    logging.info(f'Attention Out MAE: {mean_absolute_fraction_error(attn_out, tt_attn_out_cpu):.3f}')
 
     '''
-    Reshape, dense layer
-    nh, s, dh -> s, dm
+    Dense Layer
     '''
+    # PyTorch
     attn_out = attn_out.permute(1, 0, 2).reshape(1, s, dm)
     dense = torch.randn(1, dm, dm)
     dense_out = torch.matmul(attn_out, dense)
 
+    # TinyTensor
     tt_dense = tt_tensor(block_size, simd, torch_tensor=dense, dtype=dtype).to_device(0, dense)
     tt_attn_out = tt_attn_out.swapaxes(-2, -3).reshape((1, s // block_size, dm // block_size))
     tt_dense_out = ttf.matmul(tt_attn_out, tt_dense, op_dtype=op_dtype, runtime=runtime)
+    tt_dense_out_cpu = tt_dense_out.from_device(0)
 
-    print('dense out MAFE:', mean_absolute_fraction_error(dense_out, tt_dense_out.from_device(0)))
-
+    logging.info(f'Dense Out MAE: {mean_absolute_fraction_error(dense_out, tt_dense_out_cpu):.3f}')
 
     '''
     Residual & LayerNorm
     '''
-    attn_res = x + dense_out
+    # PyTorch
+    attn_res = torch.add(x, dense_out)
 
     gamma_ln2 = torch.randn(normalized_shape)
     beta_ln2 = torch.randn(normalized_shape)
     attn_res_norm = torch.nn.functional.layer_norm(attn_res, gamma_ln2.size(), weight=gamma_ln2, bias=beta_ln2, eps=1e-05)
 
-    gamma_ln2_expand = gamma_ln2.expand(x.size()) # I think we need to make this the same size as input for tt_functional
+    # TinyTensor
+    gamma_ln2_expand = gamma_ln2.expand(x.size())
     tt_gamma_ln2 = tt_tensor(block_size, simd, torch_tensor=gamma_ln2_expand, dtype=dtype).to_device(0, gamma_ln2_expand)
     beta_ln2_expand = beta_ln2.expand(x.size())
     tt_beta_ln2 = tt_tensor(block_size, simd, torch_tensor=beta_ln2_expand, dtype=dtype).to_device(0, beta_ln2_expand)
@@ -159,20 +168,22 @@ def tt_gpt2(target_arch):
     tt_attn_res_norm = ttf.layer_norm(tt_attn_res, beta=tt_beta_ln2, gamma=tt_gamma_ln2, op_dtype=op_dtype, runtime=runtime, fold_factors=(1,1,1))
     tt_attn_res_norm_cpu = tt_attn_res_norm.from_device(0)
 
-    print('Residual Norm MAFE:', mean_absolute_fraction_error(attn_res_norm, tt_attn_res_norm_cpu))
+    logging.info(f'Residual Norm MAE: {mean_absolute_fraction_error(attn_res_norm, tt_attn_res_norm_cpu):.3f}')
     
 
     '''
-    MLP & residual
+    MLP & Residual
     '''
+    # PyTorch
     ff1 = torch.randn(1, dm, 4*dm)
     ff2 = torch.randn(1, 4*dm, dm)
 
     ff1_out = torch.matmul(attn_res_norm, ff1)
     gelu_out = torch.nn.functional.gelu(ff1_out)
     ff2_out = torch.matmul(gelu_out, ff2)
-    gpt2_out = attn_res + ff2_out
+    gpt2_out = torch.add(attn_res, ff2_out)
 
+    # TinyTensor
     tt_ff1 = tt_tensor(block_size, simd, torch_tensor=ff1, dtype=dtype).to_device(0, ff1)
     tt_ff2 = tt_tensor(block_size, simd, torch_tensor=ff2, dtype=dtype).to_device(0, ff2)
 
@@ -183,8 +194,7 @@ def tt_gpt2(target_arch):
 
     tt_gpt2_out_cpu = tt_gpt2_out.from_device(0)
 
-    print('gpt2 decoder out MAFE:', mean_absolute_fraction_error(gpt2_out, tt_gpt2_out_cpu))
-
+    logging.info(f'GPT2 decoder output MAE: {mean_absolute_fraction_error(gpt2_out, tt_gpt2_out_cpu):.3f}')
 
 
     be_api.finish_child_process()
@@ -192,6 +202,7 @@ def tt_gpt2(target_arch):
 
 
 def main():
+    logging.basicConfig(level="INFO")
     parser = ArgumentParser()
     parser.add_argument('--device', '-d', help='Device: {wh, gs}', default='wh')
     args = parser.parse_args()
